a7d0e21db5b94b871f425d0beebfa64c9ea586c1
Guumaster <gustavoandresmarin@gmail.com>
2016-06-05 12:58:44 +0200

    config updated

--- scripts/tables_to_json.js
+++ scripts/tables_to_json.js
@@ -1,3 +1,12 @@
 'use strict'
 
+const fs = require('fs')
 
+const FILE = process.argv[2]
+
+console.log('FILE', FILE)
+const content = fs.readFileSync(FILE).toString().replace(/\r\n/gm, '\n')
+
+console.log('cont', JSON.stringify({
+  content
+}))
